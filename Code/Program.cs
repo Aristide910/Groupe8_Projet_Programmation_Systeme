fb8c73ee1c838ebb9adebe814d600836ee2242b6
Aristide910 <58232272+Aristide910@users.noreply.github.com>
2021-12-30 22:35:55 +0100

    Add files via upload

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Controller controller = new Controller();
+        }
+    }
+}
